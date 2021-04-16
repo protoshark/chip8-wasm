9e30ef33b8ffb8d8f8b38c69f70bf10b812cb7fd
Protoshark <protoshark@pm.me>
2021-04-16 16:25:54 -0300

    Change axios baseURL

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import {render} from "react-dom";
+import { render } from "react-dom";
+import axios from "axios";
+
 
 import "./index.scss";
 
 import Root from "./Root";
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://protoshark.github.io/chip8-wasm"
+    : "";
 
 render(<Root />, document.getElementById("root"));
